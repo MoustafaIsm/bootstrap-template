62027351a0db2935802accb4e2540053deae9fb6
Moustafa <moustafaism013@hotmail.com>
2022-09-13 22:54:07 +0300

    Finished the frontend for the messages table

--- scripts/messages_script.js
+++ scripts/messages_script.js
@@ -3,5 +3,5 @@ const table = document.getElementById("messages-table");
 fetch("http://localhost/SEF/bootstrap-template-backend/get_messages.php")
     .then((response) => response.body)
     .then((data) => {
-
+        console.log(data);
     });
